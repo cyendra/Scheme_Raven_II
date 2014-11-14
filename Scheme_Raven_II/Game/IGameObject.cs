c106c6840211f763ba1b571750bcacb83994a1ea
cyendra <g1483335912@gmail.com>
2014-11-14 18:01:21 +0800

    st

---
+++ Scheme_Raven_II/Game/IGameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheme_Raven_II.Game
+{
+    interface IGameObject
+    {
+        void Update(double elapsedTime);
+        void Render();
+    }
+}
